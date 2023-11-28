d6540aac377097491e39dbde61edc362627dc399
tqso <interesting0715@gmail.com>
2023-11-28 13:19:25 +0900

    fix

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,12 @@
 const lines = document.getElementsByClassName("result_list_line");
 
 if (lines.length > 0) {
-    for (let i = 0; i < lines.length; i++) {
-      let course_name = lines[i].getElementsByClassName("tasklist-course")
-      const task_name = lines[i].getElementsByClassName("tasklist-title")
-      let task_url = task_name[0].getElementsByTagName("a")[0].href
-      let task_id = task_url.split("/")[6].split("?")[1].split("&")[0].split("=")[1]
-      base_url = "https://scombz.shibaura-it.ac.jp/lms/course?idnumber="
-      course_name[0].innerHTML = `<a href="${base_url}${task_id}">${course_name[0].innerHTML}</a>` 
-    }
+  for (let i = 0; i < lines.length; i++) {
+    let course_name = lines[i].getElementsByClassName("tasklist-course")
+    const task_name = lines[i].getElementsByClassName("tasklist-title")
+    let task_url = task_name[0].getElementsByTagName("a")[0].href
+    let task_id = task_url.split("/")[6].split("?")[1].split("&")[0].split("=")[1]
+    base_url = "https://scombz.shibaura-it.ac.jp/lms/course?idnumber="
+    course_name[0].innerHTML = `<a href="${base_url}${task_id}">${course_name[0].innerHTML}</a>` 
+  }
 }
